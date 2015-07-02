cabac194228d8a6e5194e689b935ebf826d130b8
Mauro Servienti <mauro@topics.it>
2015-07-02 14:20:19 +0200

    ability to create error and audit queues only

--- QueueCreator/Program.cs
+++ QueueCreator/Program.cs
@@ -9,131 +9,146 @@ using Topics.Radical.Helpers;
 
 namespace QueueCreator
 {
-	class Program
-	{
-		static void Main( string[] args )
-		{
-			var cmdLine = CommandLine.GetCurrent();
-			var prms = cmdLine.As<Parameters>();
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var cmdLine = CommandLine.GetCurrent();
+            var prms = cmdLine.As<Parameters>();
 
-			var qn = @".\private$\" + prms.QueueName;
+            if( !String.IsNullOrWhiteSpace( prms.QueueName ) )
+            {
+                var qn = @".\private$\" + prms.QueueName;
 
-			if ( !MessageQueue.Exists( qn ) )
-			{
-				var q = MessageQueue.Create( qn, true );
-				SetPermissions( q, prms );
+                if( !MessageQueue.Exists( qn ) )
+                {
+                    var q = MessageQueue.Create( qn, true );
+                    SetPermissions( q, prms );
 
-				if ( prms.CreateSubscriptionsQueue )
-				{
-					var s = MessageQueue.Create( qn + ".subscriptions", true );
-					SetPermissions( s, prms );
-				}
-
-				if ( prms.CreateTimeoutsQueue )
-				{
-                    //v3.x
-                    var name = ".timeouts";
-
-                    var t = MessageQueue.Create( qn + name, true );
-                    SetPermissions( t, prms );
-
-                    if ( prms.CompatibilityLevel.StartsWith( "v4", StringComparison.OrdinalIgnoreCase ) ) 
+                    if( prms.CreateSubscriptionsQueue )
                     {
-                        name = ".TimeoutsDispatcher";
-
-                        var td = MessageQueue.Create( qn + name, true );
-                        SetPermissions( td, prms );
+                        var s = MessageQueue.Create( qn + ".subscriptions", true );
+                        SetPermissions( s, prms );
                     }
-				}
 
-				if ( prms.CreateRetriesQueue )
-				{
-					var r = MessageQueue.Create( qn + ".retries", true );
-					SetPermissions( r, prms );
-				}
+                    if( prms.CreateTimeoutsQueue )
+                    {
+                        //v3.x
+                        var name = ".timeouts";
 
-                if ( prms.CreateErrorQueue )
-                {
-                    var r = MessageQueue.Create( qn + "error", true );
-                    SetPermissions( r, prms );
+                        var t = MessageQueue.Create( qn + name, true );
+                        SetPermissions( t, prms );
+
+                        if( prms.CompatibilityLevel.StartsWith( "v4", StringComparison.OrdinalIgnoreCase ) )
+                        {
+                            name = ".TimeoutsDispatcher";
+
+                            var td = MessageQueue.Create( qn + name, true );
+                            SetPermissions( td, prms );
+                        }
+                    }
+
+                    if( prms.CreateRetriesQueue )
+                    {
+                        var r = MessageQueue.Create( qn + ".retries", true );
+                        SetPermissions( r, prms );
+                    }
                 }
+            }
 
-                if ( prms.CreateAuditQueue )
-                {
-                    var r = MessageQueue.Create( qn + "audit", true );
-                    SetPermissions( r, prms );
-                }
-			}
-		}
+            if( prms.CreateErrorQueue )
+            {
+                var r = MessageQueue.Create( @".\private$\error", true );
+                SetPermissions( r, prms );
+            }
 
-		static string GetLocalizedName(WellKnownSidType sidType) 
-		{
-			var sid = new SecurityIdentifier( WellKnownSidType.WorldSid, null );
-			var reference = sid.Translate( typeof( System.Security.Principal.NTAccount ) );
-			var name = reference.ToString();
+            if( prms.CreateAuditQueue )
+            {
+                var r = MessageQueue.Create( @".\private$\audit", true );
+                SetPermissions( r, prms );
+            }
 
-			return name;
-		}
+            if( prms.CreateErrorLogQueue )
+            {
+                var r = MessageQueue.Create( @".\private$\error.log", true );
+                SetPermissions( r, prms );
+            }
 
-		static void SetPermissions( MessageQueue q, Parameters prms )
-		{
-			var administrators = GetLocalizedName( WellKnownSidType.BuiltinAdministratorsSid );
-			var everyone = GetLocalizedName( WellKnownSidType.WorldSid );
+            if( prms.CreateAuditLogQueue )
+            {
+                var r = MessageQueue.Create( @".\private$\audit.log", true );
+                SetPermissions( r, prms );
+            }
+        }
+
+        static string GetLocalizedName( WellKnownSidType sidType )
+        {
+            var sid = new SecurityIdentifier( WellKnownSidType.WorldSid, null );
+            var reference = sid.Translate( typeof( System.Security.Principal.NTAccount ) );
+            var name = reference.ToString();
+
+            return name;
+        }
+
+        static void SetPermissions( MessageQueue q, Parameters prms )
+        {
+            var administrators = GetLocalizedName( WellKnownSidType.BuiltinAdministratorsSid );
+            var everyone = GetLocalizedName( WellKnownSidType.WorldSid );
             var anonymous = GetLocalizedName( WellKnownSidType.AnonymousSid );
 
-			q.SetPermissions( "SYSTEM", MessageQueueAccessRights.FullControl, AccessControlEntryType.Allow );
-			q.SetPermissions( administrators, MessageQueueAccessRights.FullControl, AccessControlEntryType.Allow );
-			q.SetPermissions( anonymous, MessageQueueAccessRights.WriteMessage, AccessControlEntryType.Allow );
-			q.SetPermissions( everyone, MessageQueueAccessRights.WriteMessage, AccessControlEntryType.Allow );
-			q.SetPermissions( everyone, MessageQueueAccessRights.GetQueueProperties, AccessControlEntryType.Allow );
+            q.SetPermissions( "SYSTEM", MessageQueueAccessRights.FullControl, AccessControlEntryType.Allow );
+            q.SetPermissions( administrators, MessageQueueAccessRights.FullControl, AccessControlEntryType.Allow );
+            q.SetPermissions( anonymous, MessageQueueAccessRights.WriteMessage, AccessControlEntryType.Allow );
+            q.SetPermissions( everyone, MessageQueueAccessRights.WriteMessage, AccessControlEntryType.Allow );
+            q.SetPermissions( everyone, MessageQueueAccessRights.GetQueueProperties, AccessControlEntryType.Allow );
 
-			if ( prms.AddCurrentWindowsUser )
-			{
-				var identity = WindowsIdentity.GetCurrent();
-				var name = identity.Name;
-				q.SetPermissions( name, MessageQueueAccessRights.FullControl, AccessControlEntryType.Allow );
-			}
+            if( prms.AddCurrentWindowsUser )
+            {
+                var identity = WindowsIdentity.GetCurrent();
+                var name = identity.Name;
+                q.SetPermissions( name, MessageQueueAccessRights.FullControl, AccessControlEntryType.Allow );
+            }
 
-			if ( !String.IsNullOrWhiteSpace( prms.CustomUsers ) ) 
-			{
-				var users = prms.CustomUsers.Split( new[] { ',' }, StringSplitOptions.RemoveEmptyEntries )
-					.Select( u => u.Trim() );
+            if( !String.IsNullOrWhiteSpace( prms.CustomUsers ) )
+            {
+                var users = prms.CustomUsers.Split( new[] { ',' }, StringSplitOptions.RemoveEmptyEntries )
+                    .Select( u => u.Trim() );
 
-				foreach ( var user in users ) 
-				{
-					q.SetPermissions( user, MessageQueueAccessRights.FullControl, AccessControlEntryType.Allow );
-				}
-			}
-		}
-	}
+                foreach( var user in users )
+                {
+                    q.SetPermissions( user, MessageQueueAccessRights.FullControl, AccessControlEntryType.Allow );
+                }
+            }
+        }
+    }
 
-	class Parameters
-	{
-		public Parameters()
-		{
-			this.CreateRetriesQueue = true;
-			this.CreateSubscriptionsQueue = true;
-			this.CreateTimeoutsQueue = true;
+    class Parameters
+    {
+        public Parameters()
+        {
+            this.CreateRetriesQueue = true;
+            this.CreateSubscriptionsQueue = true;
+            this.CreateTimeoutsQueue = true;
             this.CompatibilityLevel = "v4";
-		}
+        }
 
-		[CommandLineArgument( "Name", Aliases = new[] { "n" } )]
-		public String QueueName { get; set; }
+        [CommandLineArgument( "Name", IsRequired = false, Aliases = new[] { "n" } )]
+        public String QueueName { get; set; }
 
-		[CommandLineArgument( "CreateRetriesQueue", IsRequired = false, Aliases = new[] { "rq" } )]
-		public Boolean CreateRetriesQueue { get; set; }
+        [CommandLineArgument( "CreateRetriesQueue", IsRequired = false, Aliases = new[] { "rq" } )]
+        public Boolean CreateRetriesQueue { get; set; }
 
-		[CommandLineArgument( "CreateSubscriptionsQueue", IsRequired = false, Aliases = new[] { "sq" } )]
-		public Boolean CreateSubscriptionsQueue { get; set; }
+        [CommandLineArgument( "CreateSubscriptionsQueue", IsRequired = false, Aliases = new[] { "sq" } )]
+        public Boolean CreateSubscriptionsQueue { get; set; }
 
-		[CommandLineArgument( "CreateTimeoutsQueue", IsRequired = false, Aliases = new[] { "tq" } )]
-		public Boolean CreateTimeoutsQueue { get; set; }
+        [CommandLineArgument( "CreateTimeoutsQueue", IsRequired = false, Aliases = new[] { "tq" } )]
+        public Boolean CreateTimeoutsQueue { get; set; }
 
-		[CommandLineArgument( "AddCurrentWindowsUser", IsRequired = false, Aliases = new[] { "acwu" } )]
-		public Boolean AddCurrentWindowsUser { get; set; }
+        [CommandLineArgument( "AddCurrentWindowsUser", IsRequired = false, Aliases = new[] { "acwu" } )]
+        public Boolean AddCurrentWindowsUser { get; set; }
 
-		[CommandLineArgument( "CustomUsers", IsRequired = false, Aliases = new[] { "users" } )]
-		public String CustomUsers { get; set; }
+        [CommandLineArgument( "CustomUsers", IsRequired = false, Aliases = new[] { "users" } )]
+        public String CustomUsers { get; set; }
 
         [CommandLineArgument( "CompatibilityLevel", IsRequired = false, Aliases = new[] { "cl" } )]
         public String CompatibilityLevel { get; set; }
@@ -143,5 +158,11 @@ namespace QueueCreator
 
         [CommandLineArgument( "CreateAudiQueue", IsRequired = false, Aliases = new[] { "audit" } )]
         public Boolean CreateAuditQueue { get; set; }
-	}
+
+        [CommandLineArgument( "CreateErrorLogQueue", IsRequired = false, Aliases = new[] { "error.log" } )]
+        public Boolean CreateErrorLogQueue { get; set; }
+
+        [CommandLineArgument( "CreateAudiLogQueue", IsRequired = false, Aliases = new[] { "audit.log" } )]
+        public Boolean CreateAuditLogQueue { get; set; }
+    }
 }
